2a7c213cfca9240717935ac042a958592fa70ade
Lord225 <zlotymaciej@gmail.com>
2023-01-17 20:44:57 +0100

    wegerghtrehfewfewfew

---
+++ battleship_client/src/main/java/com/example/battleship_client/model/Message.java
@@ -0,0 +1,34 @@
+package com.example.battleship_client.model;
+
+import com.google.gson.Gson;
+import java.util.Map;
+
+public class Message {
+    private static final Gson gson = new Gson();
+    public String content;
+    public Map<String,String> adds;
+    public Message(String content, Map<String,String> additionalContent) {
+        this.content = content;
+        this.adds = additionalContent;
+    }
+
+    public static Message newMessage(String message) {
+        return new Message("message", Map.of("message", message));
+    }
+
+    public static Message fromJson(String json) {
+        return gson.fromJson(json, Message.class);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public String toJson() {
+        return gson.toJson(this);
+    }
+}
+
+
+
