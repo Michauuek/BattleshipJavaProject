596b811d5148f114d6a70635b70a30044d9a7756
michal <minauka@tuta.io>
2023-01-20 10:29:02 +0100

    database add game

--- battleship_server/src/main/java/com/example/data/DatabaseFactory.java
+++ battleship_server/src/main/java/com/example/data/DatabaseFactory.java
@@ -22,10 +22,34 @@ public class DatabaseFactory {
             e.printStackTrace();
         } //close connection
     }
-    public static void addUser(String nickname){
-        var date = LocalDateTime.now();
+    public static int addUser(String nickname){
         var insertSql = "INSERT INTO users(name)"
                 + " VALUES('" + nickname +"')";
+        try {
+            var affectedRows = statement.executeUpdate(insertSql,  Statement.RETURN_GENERATED_KEYS);
+
+            if (affectedRows == 0) {
+                throw new SQLException("Creating user failed, no rows affected.");
+            }
+
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    return generatedKeys.getInt(1);
+                }
+                else {
+                    throw new SQLException("Creating user failed, no ID obtained.");
+                }
+            }
+        }
+        catch(SQLException e){
+            System.out.println(e);
+        }
+        return -1;
+    }
+
+    public static void addGame(int winner, int loser){
+        Timestamp tsmp = new Timestamp(System.currentTimeMillis());
+        var insertSql = "INSERT INTO games(winner, loser, game_date) VALUES("+winner+","+loser+",'"+tsmp+"')";
         try {
             statement.executeUpdate(insertSql);
         }
