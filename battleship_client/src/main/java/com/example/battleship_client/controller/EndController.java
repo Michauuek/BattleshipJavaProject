4e015c4d592f418a8720f911194885acd264cb86
Michauuek <michal.rzemieniuk@gmail.com>
2023-01-25 14:46:24 +0100

    Add search new game

--- battleship_client/src/main/java/com/example/battleship_client/controller/EndController.java
+++ battleship_client/src/main/java/com/example/battleship_client/controller/EndController.java
@@ -3,9 +3,13 @@ package com.example.battleship_client.controller;
 import com.example.battleship_client.model.Game;
 import com.example.battleship_client.networking.DataReader;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -13,9 +17,12 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 
@@ -24,6 +31,8 @@ public class EndController implements Initializable {
     private Label messageLabel;
     @FXML
     private VBox rankingVbox;
+    @FXML
+    private Button newGameButton;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -36,13 +45,21 @@ public class EndController implements Initializable {
         var num = 1;
         for(var game : games){
             var hbox = createHBox();
-            var text = createText(num + ". " + game.getWinner() + " vs " + game.getLoser() + "at " + game.getDate());
+            var text = createText(num + ". " + game.getWinner() + " vs " + game.getLoser() + " at " + game.getDate());
 
             TextFlow textFlow = new TextFlow(text);
             hbox.getChildren().add(textFlow);
             rankingVbox.getChildren().add(hbox);
             num++;
         }
+    }
+
+    @FXML
+    public void searchNewGame() throws IOException {
+        Stage stage = (Stage) newGameButton.getScene().getWindow();
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/GameSetupView.fxml")));
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 
     private HBox createHBox() {
